fedf9380fdd0fd807de80c2d4fbc8ae15c69543b
Sofia Toro <laura.toro01@uceva.edu.co>
2024-11-15 10:41:54 -0500

    facultad components html..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FacultadesComponent } from './facultades/facultades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FacultadesComponent
   ],
   imports: [
     BrowserModule,
