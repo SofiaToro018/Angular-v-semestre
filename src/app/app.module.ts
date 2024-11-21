9802fe8c0810a83a888c1e778d2ff7819bbb426b
Richard Rivera <richard.rivera01@uceva.edu.co>
2024-11-20 23:27:47 -0500

    segundo intento de resolución de conflictos y cambios en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListarOvasComponentCssComponent } from './ovas/listar-ovas/listar-ovas.component.css/listar-ovas.component.css.component';
 import { ListarOvasComponent } from './ovas/listar-ovas/listar-ovas.component';
+import { CrearOvaComponent } from './ova/crear-ova/crear-ova.component';
+import { DetalleOvaComponent } from './ova/detalle-ova/detalle-ova.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListarOvasComponentCssComponent,
-    ListarOvasComponent
+    ListarOvasComponent,
+    CrearOvaComponent,
+    DetalleOvaComponent
   ],
   imports: [
     BrowserModule,
